5dbd71c87de4ada9aa56d4b0f50a7343a75d694b
kaitoiha <kaitooooo18@gmail.com>
2023-11-12 21:46:15 +0900

    アップデート処理追加

--- resources/views/admin/owners/edit.blade.php
+++ resources/views/admin/owners/edit.blade.php
@@ -12,6 +12,7 @@
                     <section class="text-gray-600 body-font relative">
                         <div class="container px-5 mx-auto">
                             <form method="post" action="{{ route('admin.owners.update', ['owner' => $owner->id]) }}">
+                                @method('put')
                                 @csrf
                                 <div class="flex flex-col text-center w-full mb-12">
                                     <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">オーナー情報編集</h1>
