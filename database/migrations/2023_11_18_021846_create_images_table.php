7e895c2d4684c63c90309223e18adf32d0bd39cd
kaitoiha <kaitooooo18@gmail.com>
2023-11-18 11:58:08 +0900

    マイグレーションでテーブル追加

--- database/migrations/2023_11_18_021846_create_images_table.php
+++ database/migrations/2023_11_18_021846_create_images_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('owner_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->string('filename');
+            $table->string('title')->nullable();
             $table->timestamps();
         });
     }
