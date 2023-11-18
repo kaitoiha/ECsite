662f09fd3fe51b97588c7d66f732a126ee59d2d5
kaitoiha <kaitooooo18@gmail.com>
2023-11-18 14:07:45 +0900

    画像の幅を全て揃える

--- resources/views/components/thumbnail.blade.php
+++ resources/views/components/thumbnail.blade.php
@@ -11,6 +11,6 @@
     @if(empty($filename))
         <img src="{{asset('images/no_image.jpg')}}">
     @else
-        <img src="{{asset($path.$filename)}}">
+        <img src="{{asset($path.$filename)}}" class="h-40 mx-auto object-cover">
     @endif
 </div>
