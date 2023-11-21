9d25d60658cb1e8380c8ed2c920893418f41fc19
kaitoiha <kaitooooo18@gmail.com>
2023-11-21 21:49:24 +0900

    ログアウト時のリダイレクト先修正

--- routes/owner.php
+++ routes/owner.php
@@ -25,9 +25,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('owner.welcome');
-});
+// Route::get('/', function () {
+//     return view('owner.welcome');
+// });
 
 Route::prefix('shops')->
     middleware('auth:owners')->group(function () {
