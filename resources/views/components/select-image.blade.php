d0a7bf9efeeb75aab846b3c4d1357db10bcca709
kaitoiha <kaitooooo18@gmail.com>
2023-11-21 19:15:52 +0900

    Product Store

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -19,7 +19,7 @@
             @foreach ($images as $image)
                 <div class="w-1/4 p-2 md:p-4">
                     <div class="border rounded-md p-2 md:p-4">
-                        <img class="image h-40 mx-auto object-cover" data-id="{{ $name }}_{{ $image->id }}"
+                        <img class="image" data-id="{{ $name }}_{{ $image->id }}"
                         data-file="{{ $image->filename }}"
                         data-path="{{ asset('storage/products/') }}"
                         data-modal="{{ $modal }}"
