99f5d09d6524c684f6d9114ab2b70a9b32bb62de
kaitoiha <kaitooooo18@gmail.com>
2023-11-25 20:22:27 +0900

    is_sellingがfalseなら404

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,13 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
-use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('item');
+
+            if (!is_null($id)) {
+                $itemId = Product::availableItems()->where('products.id', $id)->exists();
+                if (!$itemId) {
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
     }
 
     public function index()
