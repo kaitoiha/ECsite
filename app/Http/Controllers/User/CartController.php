1cf958018c16a297d9ac776d7de304fe76220806
kaitoiha <kaitooooo18@gmail.com>
2023-11-25 14:11:33 +0900

    cart viewを作成

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -20,7 +20,7 @@ class CartController extends Controller
             $totalPrice += $product->price * $product->pivot->quantity;
         }
 
-        return view('user.cart.index',
+        return view('user.cart',
             compact('products', 'totalPrice'));
     }
 
