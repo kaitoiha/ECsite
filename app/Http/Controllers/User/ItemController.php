d36e1e73a2a9e0e7eb9a62587e964b502c880d79
kaitoiha <kaitooooo18@gmail.com>
2023-11-26 13:58:47 +0900

    キュー&ジョブでメール送信

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -9,6 +9,7 @@ use App\Models\Stock;
 use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -32,8 +33,12 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+        // メール送信処理(同期)
+        // Mail::to('test@example.com')
+        // ->send(new TestMail());
+
+        // メール送信処理(非同期)
+        SendThanksMail::dispatch();
 
         $categories = PrimaryCategory::with('secondary')
         ->get();
