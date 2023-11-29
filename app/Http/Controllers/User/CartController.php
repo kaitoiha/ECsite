c6795be81de3b85778cd5ef3828ae4267040eee4
kaitoiha <kaitooooo18@gmail.com>
2023-11-29 19:52:11 +0900

    カート情報から配列を作成

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -9,6 +9,7 @@ use App\Models\Cart;
 use App\Models\User;
 use App\Models\Stock;
 use App\Constants\Common;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
@@ -60,6 +61,10 @@ class CartController extends Controller
 
     public function checkout()
     {
+        ///
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        ///
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
         $line_items = [];
