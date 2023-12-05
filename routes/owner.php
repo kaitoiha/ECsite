8d7bcbe4ef1e378411ce06d321d99d1df4d0ebf0
kaitoiha <kaitooooo18@gmail.com>
2023-12-05 19:21:16 +0900

    ownerのプロフィール画面追加

--- routes/owner.php
+++ routes/owner.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Owner\ShopController;
 use App\Http\Controllers\Owner\ImageController;
 use App\Http\Controllers\Owner\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -92,4 +93,10 @@ Route::middleware('auth:owners')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+});
+
+Route::middleware('auth:owners')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
