0450ba33895a983da372aeb1a17acfd538259a79
kaitoiha <kaitooooo18@gmail.com>
2024-08-12 18:12:48 +0900

    シーダー変更

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -20,28 +20,83 @@ class ImageSeeder extends Seeder
                 'title'    => 'ダミー1'
             ],
             [
-                'owner_id' => 2,
+                'owner_id' => 1,
                 'filename' => 'sample2.jpg',
                 'title'    => 'ダミー2'
             ],
             [
-                'owner_id' => 3,
+                'owner_id' => 1,
                 'filename' => 'sample3.jpg',
                 'title'    => 'ダミー3'
             ],
             [
-                'owner_id' => 4,
+                'owner_id' => 1,
                 'filename' => 'sample4.jpg',
                 'title'    => 'ダミー4'
             ],
             [
-                'owner_id' => 5,
+                'owner_id' => 1,
                 'filename' => 'sample5.jpg',
                 'title'    => 'ダミー5'
             ],
             [
-                'owner_id' => 6,
+                'owner_id' => 1,
                 'filename' => 'sample6.jpg',
+                'title'    => 'ダミー6'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'm1.jpg',
+                'title'    => 'ダミー1'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'm2.jpg',
+                'title'    => 'ダミー2'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'm3.jpg',
+                'title'    => 'ダミー3'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'm4.jpg',
+                'title'    => 'ダミー4'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'm5.jpg',
+                'title'    => 'ダミー5'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'seed1.jpg',
+                'title'    => 'ダミー6'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'seed2.jpg',
+                'title'    => 'ダミー6'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'seed3.jpg',
+                'title'    => 'ダミー6'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'seed4.jpg',
+                'title'    => 'ダミー6'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'seed5.jpg',
+                'title'    => 'ダミー6'
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'seed6.jpg',
                 'title'    => 'ダミー6'
             ],
         ]);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="robots" content="noindex">
+        <title>@yield('title', '植物ショップ')</title>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
